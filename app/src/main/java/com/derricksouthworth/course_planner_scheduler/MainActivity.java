843a1a7e80575861349bcc3d877def22312d538c
Derrick <derricksouthworth@gmail.com>
2020-08-03 21:30:51 -0700

    initial commit

---
+++ app/src/main/java/com/derricksouthworth/course_planner_scheduler/MainActivity.java
@@ -0,0 +1,14 @@
+package com.derricksouthworth.course_planner_scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
